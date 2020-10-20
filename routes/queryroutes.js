18a1629963f441549f46c66158c20d7d376995ab
Keenan Troll <alaskatroll@gmail.com>
2020-10-19 22:01:29 -0800

    adjusted debug logging

--- routes/queryroutes.js
+++ routes/queryroutes.js
@@ -8,8 +8,10 @@ exports.list = async function(req, res){
     });
 }
 exports.search = async function(req, res){
-    console.log(`search request. filters:\n${req.body}`);
+    console.log('search request');
+    console.log(req.body);
     query = `SELECT * FROM UserRecords WHERE city = '${req.body.City}' AND state = '${req.body.State}' AND country = '${req.body.Country}'`;
+    //console.log(query);
     if(req.body.Blood_type != '') query += ` AND bloodType = "${req.body.Blood_type}"`;
     pool.query(query, async function(error, results, fields){
         if(error){
