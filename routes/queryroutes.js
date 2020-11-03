a21650ac00e48d27cde7100be6164a669b47a504
keenan <alaskatroll@gmail.com>
2020-11-03 14:10:59 -0900

    stats route

--- routes/queryroutes.js
+++ routes/queryroutes.js
@@ -30,3 +30,43 @@ exports.search = async function(req, res){
     })
 
 }
+exports.stats = async function(req, res){
+    console.log('stats request:');
+    console.log(req.body);
+    query = 'SELECT bloodType, COUNT(*) AS tally FROM UserRecords ';
+    if(req.body.Country != '') query += `WHERE country = '${req.body.Country}'`;
+    if(req.body.State != '') query += ` AND state = '${req.body.State}'`;
+    if(req.body.City != '') query += ` AND city = '${req.body.City}'`;
+    query += ' GROUP BY bloodType';
+    console.log(query);
+    stats = {
+        'A+':0,
+        'A-':0,
+        'B+':0,
+        'B-':0,
+        'O+':0,
+        'O-':0,
+        'AB+':0,
+        'AB-':0
+    }
+    pool.query(query, async function(error, results, fields){
+        if(error){
+            console.error(error);
+            res.send({
+                'code': 501,
+                'error': error
+            })
+
+        }else{
+            for (record in results){
+                row = results[record];
+                stats[row['bloodType']] = row['tally']
+            }
+            console.log(stats);
+            res.send({
+                'code':200,
+                'stats':stats
+            })
+        }
+    })
+}
